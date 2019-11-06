d1668f72cd81e3b42399661c53b3f5b1ce43a8f9
mehrabrafi143 <maharabrafi@gmail.com>
2019-11-06 13:15:31 +0600

    Updates

---
+++ precache-manifest.330fcbbeed0e897bcd391b324591be87.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a9b1ca291ac4018b4831299a6c2a9a2",
+    "url": "/edu-hive/index.html"
+  },
+  {
+    "revision": "dbf2a8bee87f43aa255c",
+    "url": "/edu-hive/static/css/2.33ab6a50.chunk.css"
+  },
+  {
+    "revision": "e378f691bb8726f53ad1",
+    "url": "/edu-hive/static/css/main.422aad7f.chunk.css"
+  },
+  {
+    "revision": "dbf2a8bee87f43aa255c",
+    "url": "/edu-hive/static/js/2.5105bbf0.chunk.js"
+  },
+  {
+    "revision": "e378f691bb8726f53ad1",
+    "url": "/edu-hive/static/js/main.e0da44af.chunk.js"
+  },
+  {
+    "revision": "e1b21db152e28e1c374c",
+    "url": "/edu-hive/static/js/runtime-main.d23404d6.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/edu-hive/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/edu-hive/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/edu-hive/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/edu-hive/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/edu-hive/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "da28786a0427b362cb4951f63d3b67fc",
+    "url": "/edu-hive/static/media/graphic.da28786a.png"
+  },
+  {
+    "revision": "833c92d9dbe731aab1e1d45607c2282a",
+    "url": "/edu-hive/static/media/icon_1.833c92d9.png"
+  },
+  {
+    "revision": "f64bfd0d1c35726e407be8adcaf70d61",
+    "url": "/edu-hive/static/media/icon_4.f64bfd0d.png"
+  },
+  {
+    "revision": "843fbed65954cd52446b4672d36bb941",
+    "url": "/edu-hive/static/media/img.843fbed6.png"
+  },
+  {
+    "revision": "77193204c2be7779359c7d2d174a2ab9",
+    "url": "/edu-hive/static/media/img1.77193204.png"
+  },
+  {
+    "revision": "16b070c6d4ff83c51654a788e97c8bdd",
+    "url": "/edu-hive/static/media/img3.16b070c6.png"
+  },
+  {
+    "revision": "4c08f3d5c70d725e7eb2f7b79a607377",
+    "url": "/edu-hive/static/media/img4.4c08f3d5.png"
+  },
+  {
+    "revision": "ec13b572316b0ab6957c341b8964d34f",
+    "url": "/edu-hive/static/media/img_1.ec13b572.png"
+  },
+  {
+    "revision": "2d836a43103201b13a51f75de0ed95ee",
+    "url": "/edu-hive/static/media/img_2.2d836a43.png"
+  },
+  {
+    "revision": "8f988cadf1dbd27f2cca18a409432418",
+    "url": "/edu-hive/static/media/img_3.8f988cad.png"
+  }
+]);
